c06217bbb9b6651d91bbb10dcf0f07b0536432cc
Sergey Brazgun <sampad777@gmail.com>
2016-11-19 12:50:00 +0300

    first commit

--- public/index.php
+++ public/index.php
@@ -24,7 +24,7 @@ echo $user->full_name();
         echo "Name: ".$user['first_name']." ".$user['last_name']."<br /><br />";
     }
 
-    echo "Добавим новые изменения в файл";
+    echo "Add changes to file";
 
 
 ?>
